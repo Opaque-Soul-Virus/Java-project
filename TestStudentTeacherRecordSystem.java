597bd72ed8d337ac9a7927bca045373bda0b2fef
Sifat Zaman <168849059+Opaque-Soul-Virus@users.noreply.github.com>
2025-01-01 19:54:39 +0600

    Delete TestStudentTeacherRecordSystem.java

--- TestStudentTeacherRecordSystem.java
+++
@@ -1,8 +0,0 @@
-package StudentTeacherRecordSystem;
-
-public class TestStudentTeacherRecordSystem {
-    public static void main(String[] args) {
-        IDandPasswords iDandPasswords = new IDandPasswords();
-        LoginPage loginPage = new LoginPage(iDandPasswords.getLoginInfo());
-    }
-}
